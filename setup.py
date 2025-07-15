4efb3669ee8969b4147af6e14e6213a96f5994f1
Rudra Chouhan <rudrachouhan0305@gmail.com>
2025-07-15 10:46:12 +0530

    setup.py file ready

--- setup.py
+++ setup.py
@@ -11,18 +11,14 @@ from typing import List
 def get_requirements()->List[str]:
     """
     This function will return list of requirements
-    
     """
     requirement_lst:List[str]=[]
     try:
         with open('requirements.txt','r') as file:
-            #Read lines from the file
-            lines=file.readlines()
-            ## Process each line
-            for line in lines:
-                requirement=line.strip()
-                ## ignore empty lines and -e .
-                if requirement and requirement!= '-e .':
+            lines=file.readlines() # Read lines from the file
+            for line in lines: # Process each line
+                requirement=line.strip()   
+                if requirement and requirement!= '-e .': # ignore empty lines and -e .
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file not found")
